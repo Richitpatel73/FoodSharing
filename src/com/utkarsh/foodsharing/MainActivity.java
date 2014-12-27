8a30e4f7436d746db69a59773383235b6d63c383
Richitpatel73 <richitpatel73@gmail.com>
2014-12-27 10:38:36 +0530

    AlertInternetConncection

--- src/com/utkarsh/foodsharing/MainActivity.java
+++ src/com/utkarsh/foodsharing/MainActivity.java
@@ -2,9 +2,13 @@ package com.utkarsh.foodsharing;
 
 import java.util.ArrayList;
 
+import com.utkarsh.foodsharing.ConnectionDetector;
 import com.utkarsh.foodsharing.SQLite.SQLiteDatabase;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +18,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	// flag for Internet connection status
+	Boolean isInternetPresent = false;
+	// Connection detector class
+	ConnectionDetector cd;
 
 	private ListView lvMain;
 	private static final String LAT_KEY = "lat";
@@ -24,23 +32,74 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// creating connection detector class instance
+		cd = new ConnectionDetector(getApplicationContext());
+		// get Internet status
+		isInternetPresent = cd.isConnectingToInternet();
+		// check for Internet status
+		if (!isInternetPresent) {
+			// Internet connection is not present
+			// Ask user to connect to Internet
+			showAlertDialog(MainActivity.this, "No Internet Connection",
+					"You don't have internet connection. "
+					+"\n"+ "Enable it & restart the app.", false);
 
-		new FetchJSONTask(this).execute();
-		db = new SQLiteDatabase(getBaseContext());
-		lvMain = (ListView) findViewById(R.id.lvMain);
-		lvMain.setOnItemClickListener(new OnItemClickListener() {
+		} else {
 
-			@Override
-			public void onItemClick(AdapterView parent, View view,
-					int position, long id) {
+			new FetchJSONTask(this).execute();
+			db = new SQLiteDatabase(getBaseContext());
+			lvMain = (ListView) findViewById(R.id.lvMain);
+			lvMain.setOnItemClickListener(new OnItemClickListener() {
 
-				Intent intent = new Intent(getBaseContext(), FoodLocation.class);
-				intent.putExtra(LAT_KEY, (db.getAllLat()).get(position));
-				intent.putExtra(LNG_KEY, (db.getAllLng()).get(position));
-				startActivity(intent);
+				@Override
+				public void onItemClick(AdapterView parent, View view,
+						int position, long id) {
+
+					Intent intent = new Intent(getBaseContext(),
+							FoodLocation.class);
+					intent.putExtra(LAT_KEY, (db.getAllLat()).get(position));
+					intent.putExtra(LNG_KEY, (db.getAllLng()).get(position));
+					startActivity(intent);
+				}
+			});
+		}
+
+	}
+
+	/**
+	 * Function to display simple Alert Dialog
+	 * 
+	 * @param context
+	 *            - application context
+	 * @param title
+	 *            - alert dialog title
+	 * @param message
+	 *            - alert message
+	 * @param status
+	 *            - success/failure (used to set icon)
+	 * */
+	public void showAlertDialog(Context context, String title, String message,
+			Boolean status) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+		// Setting Dialog Title
+		alertDialog.setTitle(title);
+
+		// Setting Dialog Message
+		alertDialog.setMessage(message);
+
+		// Setting alert dialog icon
+		alertDialog.setIcon((status) ? R.drawable.success : R.drawable.fail);
+
+		// Setting OK Button
+		alertDialog.setButton("Exit", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			finish();
 			}
 		});
 
+		// Showing Alert Message
+		alertDialog.show();
 	}
 
 	protected void setArrayList(ArrayList<String> list) {
